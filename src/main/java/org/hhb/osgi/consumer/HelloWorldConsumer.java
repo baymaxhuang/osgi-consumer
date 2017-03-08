95982e66650330d2eacf0f94c332f1b263faa1ee
baymaxhuang <baymaxhuang@gamil.com>
2017-03-08 22:59:03 +0800

    initial commit

---
+++ src/main/java/org/hhb/osgi/consumer/HelloWorldConsumer.java
@@ -0,0 +1,34 @@
+package org.hhb.osgi.consumer;
+
+import javax.swing.Timer;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import org.hhb.osgi.provider.api.HelloWorldService;
+
+public class HelloWorldConsumer implements ActionListener {
+    private final HelloWorldService service;
+    private final Timer timer;
+
+    public HelloWorldConsumer(HelloWorldService service) {
+        super();
+
+        this.service = service;
+
+        timer = new Timer(1000, this);
+    }
+
+    public void startTimer(){
+        timer.start();
+    }
+
+    public void stopTimer() {
+        timer.stop();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        service.hello();
+    }
+}
